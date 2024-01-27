2b4dd2dcd45dae6ee69759a5ead24984f5a0b517
Iryna Klosovych <ipz221_kia@student.ztu.edu.ua>
2024-01-27 20:55:51 +0200

    I can sum numbers

---
+++ TechPrakt2024Task0/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Write first num:");
+bool ok = false;
+float num1=0, num2=0;
+while(!ok)
+{
+    ok = float.TryParse(Console.ReadLine(), out num1);
+    if(!ok) Console.WriteLine("It's not a number");
+}
+Console.WriteLine("Write second num:");
+ok = false;
+while (!ok)
+{
+    ok = float.TryParse(Console.ReadLine(), out num2);
+    if(!ok)Console.WriteLine("It's not a number");
+}
+Console.WriteLine($"{num1} + {num2} = {num1+num2}");
